b81fd765c63b50fc2fc10588436f8b993c481fe7
nickb27 <nick.brill@sentera.comt>
2021-11-11 11:35:21 -0600

    two lines

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('AWS For the Win!'));
+app.get('/', (req, res) => res.send('This has been reviewed!'));
 app.get('/', (req, res) => res.send('This has been reviewed!'));
 
 app.listen(port);
